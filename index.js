332d71496a3ec4090658660fda40b69edb312aac
Sheriffdeen Muritala Adegoke <107261843+MSHERIFF2@users.noreply.github.com>
2022-09-18 20:38:15 +0100

    Add file via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function message() {
+  alert("call this number 08170205738 for registeration");
+}
+function register() {
+  alert(
+    "visit my github page to begin your training https://github.com/MSHERIFF2/CatPhotoApp"
+  );
+}
+console.log(message);
